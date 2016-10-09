cfcf702028b6fed6b3e86a1bfd6257c1d6d28ab2
mymfzcq <mymfzcq@163.com>
2016-10-09 22:47:34 +0800

    update for c# branch

---
+++ C#_Ramup/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer_REMOTE_5680.cs
@@ -0,0 +1,137 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.my_button = new System.Windows.Forms.Button();
+            this.button_botton1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // my_button
+            // 
+            this.my_button.Location = new System.Drawing.Point(95, 1);
+            this.my_button.Name = "my_button";
+            this.my_button.Size = new System.Drawing.Size(91, 23);
+            this.my_button.TabIndex = 0;
+            this.my_button.Click += new System.EventHandler(this.my_button_Click);
+            // 
+            // button_botton1
+            // 
+            this.button_botton1.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.button_botton1.Location = new System.Drawing.Point(95, 39);
+            this.button_botton1.Name = "button_botton1";
+            this.button_botton1.Size = new System.Drawing.Size(91, 37);
+            this.button_botton1.TabIndex = 0;
+            this.button_botton1.Text = "button1_test";
+            this.button_botton1.UseVisualStyleBackColor = false;
+            this.button_botton1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "button1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(32, 111);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(128, 67);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "aaa";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3"});
+            this.listBox1.Location = new System.Drawing.Point(10, 20);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(207, 304);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(645, 38);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(217, 403);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.BackgroundImage = global::WindowsFormsApplication1.Properties.Resources.aperture;
+            this.ClientSize = new System.Drawing.Size(860, 494);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_botton1);
+            this.Controls.Add(this.my_button);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1_test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.Button my_button;
+        #endregion
+        private System.Windows.Forms.Button button_botton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        
+    }
+}
+
---
+++ C#_Ramup/WindowsFormsApplication1/WindowsFormsApplication1/Form1_BACKUP_3524.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private Thread newthread;
+        static string S1 = "";
+        static int S2 = 222;
+        static int temp = 0;
+        MethodInvoker method;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("form load event");
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format("pos x is {0},pos y is {1}", ((MouseEventArgs)e).X.ToString(), ((MouseEventArgs)e).Y.ToString()));
+        }
+
+        private void my_button_Click(object sender, EventArgs e)
+        {
+            my_button.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread thread1 = Thread.CurrentThread;
+            MessageBox.Show(thread1.ManagedThreadId.ToString());
+            //temp = (temp + 1) % 2;
+            //if (temp == 1)
+            //{
+            //    label1.Text = "button1 clicked";
+            //}
+            //else if (temp == 0)
+            //{
+            //    label1.Text = "button1";
+            //}
+            //CheckForIllegalCrossThreadCalls = false;
+            newthread = new Thread(new ParameterizedThreadStart(calc));
+            newthread.Start(this);
+
+        }
+        public void sub_calc()
+                    {
+                        Thread thread3 = Thread.CurrentThread;
+                        //MessageBox.Show(thread3.ManagedThreadId.ToString());
+
+                        this.Text = thread3.ManagedThreadId.ToString();
+                    }
+        public void calc(object o)
+        {
+            method = new MethodInvoker(sub_calc);
+            Thread thread2 = Thread.CurrentThread;
+            MessageBox.Show(newthread.ManagedThreadId.ToString());
+            for (int i = 0; i < 100000; i++)
+            {
+
+                if (((Form1)o).InvokeRequired)
+                {
+                    //MessageBox.Show(thread2.ManagedThreadId.ToString());
+
+                    this.Invoke(method);
+                    //this.Invoke(new MethodInvoker(() => ((Form1)o).Text = i.ToString()));
+
+                }
+                else
+                {
+                    ((Form1)o).Text = i.ToString();
+                }
+                if (i == 80000)
+                {
+                    newthread.Suspend();
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show(listBox1.SelectedItem.ToString());
+            using (WebClient client = new WebClient())
+            {
+                string s = client.DownloadString(url);
+            }
+        }
+    }
+}
